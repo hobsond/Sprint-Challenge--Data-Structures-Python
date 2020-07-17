702a7543e0fe999133873cc9111fd4717d630ef6
hobsond <60270270+hobsond@users.noreply.github.com>
2020-07-17 11:50:56 -0400

    Update ring_buffer.py
    Ring buffer task completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.current = 0
+    
+    def __len__(self):
+        return len(self.storage)
+    
 
     def append(self, item):
-        pass
+        if len(self) != self.capacity:
+            self.storage.append(item)
+        else:
+            if self.current >= self.capacity:
+                self.current = 0 
+            self.storage[self.current] = item
+            self.current += 1
+    
+    
 
     def get(self):
-        pass
+        return self.storage
+        
+ad = RingBuffer(3)
+
+ad.append(1)
+ad.append(2)
+ad.append(3)
+ad.append(4)
+ad.append(5)
+ad.append(7)
+ad.append(1)
+
+print(ad.get())
